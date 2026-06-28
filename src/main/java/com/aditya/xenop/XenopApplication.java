2606a932773eb91963c8fe58fb0394943bdb17e4
adityyayadav <yaditya156@gmail.com>
2026-06-29 00:59:05 +0530

    initial commit : Project Setup

---
+++ src/main/java/com/aditya/xenop/XenopApplication.java
@@ -0,0 +1,13 @@
+package com.aditya.xenop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XenopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XenopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aditya/xenop/XenopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aditya.xenop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XenopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
